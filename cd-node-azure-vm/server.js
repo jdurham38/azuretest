b32dd348f730526e8d9ed0f541cd4e90eacb780d
jdurham38 <60802210+jdurham38@users.noreply.github.com>
2023-01-26 16:35:00 -0500

    Delete cd-node-azure-vm directory

--- cd-node-azure-vm/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-//test
-// Set port
-const port = process.env.PORT || 3000;
-app.set("port", port);
-
-// Server
-app.listen(port, () => console.log(`Server running on localhost:${port}`));
